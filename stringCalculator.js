0b990910743005ff0ac96f811e51e5000f13a4f7
Neeyal <41719553+Neeyal@users.noreply.github.com>
2024-11-22 18:23:07 +0530

    feat: code cleanup and refactoring.

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,28 +1,57 @@
-function add(numbers) {
-    if (!numbers) return 0;  // Empty string case
-
-    const negativeNumbers = [];
-
-    if (numbers.startsWith('//')) {
-        const delimiterSection = numbers.split('\n')[0];
-        const delimiters = delimiterSection.slice(2).split('][').map(d => d.replace(/[\[\]]/g, ''));
-        numbers = numbers.slice(delimiterSection.length + 1);
-        
-        let regex = new RegExp('[' + delimiters.join('') + ']');
-        numbers = numbers.split(regex).join(',');
+class StringCalculator {
+    constructor() {
+      this.delimiters = [',', '\n', ';']
     }
-
-    const numArray = numbers.split(/[,\n;]/).map(Number).filter(num => num <= 1000);
-
-    numArray.forEach(num => {
-        if (num < 0) negativeNumbers.push(num);
-    });
-
-    if (negativeNumbers.length > 0) {
-        throw new Error('negatives not allowed: ' + negativeNumbers.join(', '));
+  
+    add(numbers) {
+      if (!numbers) return 0
+      this.negativeNumbers = []
+  
+      if (this._hasCustomDelimiter(numbers)) {
+        numbers = this._extractCustomDelimiter(numbers)
+      }
+  
+      const numArray = this._parseNumbers(numbers)
+      this._checkForNegatives(numArray)
+  
+      return this._sum(numArray)
     }
-
-    return numArray.reduce((a, b) => a + b, 0);
-}
-
-module.exports = add;
+  
+    _hasCustomDelimiter(numbers) {
+      return numbers.startsWith('//')
+    }
+  
+    _extractCustomDelimiter(numbers) {
+      const delimiterSection = numbers.split('\n')[0]
+      const delimiters = this._getDelimiters(delimiterSection)
+      numbers = numbers.slice(delimiterSection.length + 1)
+  
+      let regex = new RegExp('[' + delimiters.join('') + ']')
+      return numbers.split(regex).join(',')
+    }
+  
+    _getDelimiters(delimiterSection) {
+      return delimiterSection.slice(2).split('][').map(d => d.replace(/[\[\]]/g, ''))
+    }
+  
+    _parseNumbers(numbers) {
+      return numbers.split(/[,\n;]/).map(Number).filter(num => num <= 1000)
+    }
+  
+    _checkForNegatives(numArray) {
+      numArray.forEach(num => {
+        if (num < 0) this.negativeNumbers.push(num)
+      })
+  
+      if (this.negativeNumbers.length > 0) {
+        throw new Error('negatives not allowed: ' + this.negativeNumbers.join(', '))
+      }
+    }
+  
+    _sum(numArray) {
+      return numArray.reduce((a, b) => a + b, 0)
+    }
+  }
+  
+  module.exports = StringCalculator
+    
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,48 +1,62 @@
-const add = require('../stringCalculator')
+const StringCalculator = require('../stringCalculator')
 
 describe('String Calculator', () => {
-    test('should return 0 for an empty string', () => {
-      expect(add('')).toBe(0)
-    });
+  let calculator
 
-    test('should return the number itself for a single number', () => {
-        expect(add('1')).toBe(1);
-        expect(add('42')).toBe(42);
-    });
+  beforeEach(() => {
+    calculator = new StringCalculator()
+  })
 
-    test('should return the sum of two numbers', () => {
-        expect(add('1,2')).toBe(3);
-        expect(add('10,20')).toBe(30);
-    });
+  test('should return 0 for an empty string', () => {
+    expect(calculator.add('')).toBe(0)
+  })
 
-    test('should return the sum of multiple numbers', () => {
-        expect(add('1,2,3,4')).toBe(10);
-        expect(add('5,15,25')).toBe(45);
-    });
+  test('should return the number itself for a single number', () => {
+    expect(calculator.add('1')).toBe(1)
+    expect(calculator.add('42')).toBe(42)
+  })
 
-    test('should handle newlines between numbers', () => {
-        expect(add('1\n2,3')).toBe(6);
-        expect(add('4\n5\n6')).toBe(15);
-    });
+  test('should return the sum of two numbers', () => {
+    expect(calculator.add('1,2')).toBe(3)
+    expect(calculator.add('10,20')).toBe(30)
+  })
 
-    test('should support custom delimiters', () => {
-        expect(add('//;\n1;2;3')).toBe(6);
-        expect(add('//|\n4|5|6')).toBe(15);
-    });
+  test('should return the sum of multiple numbers', () => {
+    expect(calculator.add('1,2,3,4')).toBe(10)
+    expect(calculator.add('5,15,25')).toBe(45)
+  })
 
-    test('should throw an error for negative numbers', () => {
-        expect(() => add('1,-2')).toThrow('negatives not allowed: -2');
-    });
+  test('should handle newlines between numbers', () => {
+    expect(calculator.add('1\n2,3')).toBe(6)
+    expect(calculator.add('4\n5\n6')).toBe(15)
+  })
 
-    test('should throw an error for multiple negative numbers', () => {
-        expect(() => add('1,-2,-3')).toThrow('negatives not allowed: -2, -3');
-    });
+  test('should support custom delimiters', () => {
+    expect(calculator.add('//;\n1;2;3')).toBe(6)
+    expect(calculator.add('//|\n4|5|6')).toBe(15)
+  })
 
-    test('should ignore numbers greater than 1000', () => {
-        expect(add('1001,2')).toBe(2);
-    });
+  test('should throw an error for negative numbers', () => {
+    expect(() => calculator.add('1,-2')).toThrow('negatives not allowed: -2')
+  })
 
-    test('should support multiple delimiters', () => {
-        expect(add('//[;][*]\n1;2*3')).toBe(6);
-    });
+  test('should throw an error for multiple negative numbers', () => {
+    expect(() => calculator.add('1,-2,-3')).toThrow('negatives not allowed: -2, -3')
+  })
+
+  test('should ignore numbers greater than 1000', () => {
+    expect(calculator.add('1001,2')).toBe(2)
+  })
+
+  test('should support multiple delimiters', () => {
+    expect(calculator.add('//[;][*]\n1;2*3')).toBe(6)
+  })
+
+  test('should support delimiters of any length', () => {
+    expect(calculator.add('//[***]\n1***2***3')).toBe(6)
+  })
+
+  test('should support delimiters with different characters', () => {
+    expect(calculator.add('//[*][%]\n1*2%3')).toBe(6)
+  })
 })
