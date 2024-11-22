bcb475d4657f938e34a95947cebf81cd005a2b1a
Neeyal <41719553+Neeyal@users.noreply.github.com>
2024-11-22 16:03:46 +0530

    feat: support unknown number of numbers.

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,12 @@
 function add(numbers) {
     if (!numbers) return 0 // Empty string case
 
+    if (numbers.startsWith('//')) {
+        const delimiter = numbers[2];
+        numbers = numbers.slice(4);
+        return numbers.split(delimiter).map(Number).reduce((a, b) => a + b, 0);
+    }
+
     return numbers.split(/[,\n]/).map(Number).reduce((a, b) => a + b, 0);
 }
 
